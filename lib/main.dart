548017d5c5b4f912f593253da20b5307b6e8cb63
Gaurav Varadkar <gaurav.varadkar@blinkx.in>
2024-08-01 00:18:53 +0530

    formatting

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-  
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-    MyHomePage({super.key,});
+  MyHomePage({
+    super.key,
+  });
 
-
-final WebSocketChannel channel = IOWebSocketChannel.connect('wss://echo.websocket.org');
-
+  final WebSocketChannel channel =
+      IOWebSocketChannel.connect('wss://echo.websocket.org');
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -37,10 +37,10 @@ final WebSocketChannel channel = IOWebSocketChannel.connect('wss://echo.websocke
 
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController editingController = TextEditingController();
-final List<String> messageList = []; 
+  final List<String> messageList = [];
 
-void _sendMessage() {
-    if(editingController.text.isEmpty) return;
+  void _sendMessage() {
+    if (editingController.text.isEmpty) return;
     widget.channel.sink.add(editingController.text);
     editingController.clear();
   }
@@ -52,38 +52,32 @@ void _sendMessage() {
     super.dispose();
   }
 
-
- ListView getMessageList() {
+  ListView getMessageList() {
     return ListView(
-      children: messageList.map((message) => ListTile(
-        title: Container(
-          color: Colors.tealAccent,
-          padding: const EdgeInsets.all(8.0),
-          height: 50.0,
-          child: Text(message),
-        ),
-      )).toList(),
+      children: messageList
+          .map((message) => ListTile(
+                title: Container(
+                  color: Colors.tealAccent,
+                  padding: const EdgeInsets.all(8.0),
+                  height: 50.0,
+                  child: Text(message),
+                ),
+              ))
+          .toList(),
     );
   }
- 
-
-  
 
   @override
   Widget build(BuildContext context) {
-  
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
- 
         title: const Text('WebSocket Example'),
       ),
-      body:  Padding(
+      body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-         
           children: [
             Form(
               child: TextField(
@@ -91,29 +85,27 @@ void _sendMessage() {
                 decoration: const InputDecoration(labelText: 'Send message'),
               ),
             ),
-            const SizedBox(height: 24,),
+            const SizedBox(
+              height: 24,
+            ),
             Expanded(
               child: StreamBuilder(
                 stream: widget.channel.stream,
                 builder: (context, snapshot) {
-                  if(snapshot.hasData) {
+                  if (snapshot.hasData) {
                     messageList.add(snapshot.data.toString());
                   }
                   return getMessageList();
                 },
               ),
             )
-          ],),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _sendMessage,
         child: const Icon(Icons.send),
-        ),
-
+      ),
     );
   }
-
-  
 }
-
-
